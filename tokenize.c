b17508723214a6713a1050d7a31a2e15fb265cdc
Hiroki Oshima <geet1876@gmail.com>
2020-05-20 02:53:27 +0900

    アルファベット、数字、アンダースコアの文字列（Identifier）をトークンの種類情報と一緒に出力できる

--- tokenize.c
+++ tokenize.c
@@ -2,13 +2,16 @@
 #define STR(var) #var // 引数にした変数を変数名を示す文字列リテラルとして返すマクロ関数
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 // Token
 typedef enum {
-	TK_NUM, // Numeric literals
-	TK_EOF, // Enf-of-file markers
+	TK_IDENT, // Identifier
+	TK_NUM,   // Numeric literals
+	TK_EOF,   // Enf-of-file markers
 } TokenKind;
 
 typedef struct Token Token;
@@ -29,6 +32,14 @@ static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
 	return tok;
 }
 
+static bool is_alpha(char c) {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
+static bool is_alnum(char c) {
+	return is_alpha(c) || ('0' <= c && c <= '9');
+}
+
 static Token *tokenize(char *p) {
 	Token head = {};
 	Token *cur = &head;
@@ -40,6 +51,15 @@ static Token *tokenize(char *p) {
 			char *q = p;
 			cur->val = strtoul(p, &p, 10);
 			cur->len = p - q;
+			continue;
+		}
+
+		// Identifier
+		if (is_alpha(*p)) {
+			char *q = p++;
+			while (is_alnum(*p))
+				p++;
+			cur = new_token(TK_IDENT, cur, q, p - q);
 			continue;
 		}
 
@@ -63,6 +83,9 @@ int main(int argc, char **argv) {
 	while (cur->kind != TK_EOF) {
 		if (cur->kind == TK_NUM)
 			printf("token_type: %s, value: %ld\n", STR(TK_NUM), cur->val);
+
+		if (cur->kind == TK_IDENT)
+			printf("token_type: %s, identifier: %s\n", STR(TK_IDENT), strndup(cur->loc, cur->len));
 
 		cur = cur->next;
 	}
