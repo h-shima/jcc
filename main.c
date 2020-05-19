e6fcf23cff4a89fff4ff7d44fd6bd7958a2fefec
Hiroki Oshima <geet1876@gmail.com>
2020-05-20 06:11:11 +0900

    .jackファイルを一つ受け取り、正しくトークナイズしてxmlファイルを出力できる

--- main.c
+++ main.c
@@ -37,30 +37,46 @@ int main(int argc, char **argv) {
 	Token *cur = tokenize(input);
 
 	FILE *fout;
-	fout = fopen("Output.txt", "w");
+	fout = fopen("Output.xml", "w");
 	if (fout == NULL) {
 		fprintf(stderr, "出力ファイルをオープンできません");
 		exit(1);
 	}
 
+	fprintf(fout, "<tokens>\n");
+
 	while (cur->kind != TK_EOF) {
 		if (cur->kind == TK_NUM)
-			fprintf(fout, "token_type: %s, value: %ld\n", STR(TK_NUM), cur->val);
+			fprintf(fout, "<integerConstant>%ld</integerConstant>\n", cur->val);
 
 		if (cur->kind == TK_IDENT)
-			fprintf(fout, "token_type: %s, identifier: %s\n", STR(TK_IDENT), strndup(cur->loc, cur->len));
+			fprintf(fout, "<identifier>%s</identifier>\n", strndup(cur->loc, cur->len));
 
-		if (cur->kind == TK_SYMBOL)
-			fprintf(fout, "token_type: %s, symbol: %s\n", STR(TK_SYMBOL), strndup(cur->loc, 1));
+		if (cur->kind == TK_SYMBOL) {
+			// XMLで<, >, &をエスケープするため
+			if (!strncmp(cur->loc, "<", 1))
+				fprintf(fout, "<symbol>&lt;</symbol>\n");
+
+			if (!strncmp(cur->loc, ">", 1))
+				fprintf(fout, "<symbol>&gt;</symbol>\n");
+
+			if (!strncmp(cur->loc, "&", 1))
+				fprintf(fout, "<symbol>&amp;</symbol>\n");
+
+			if (strncmp(cur->loc, "<", 1) && strncmp(cur->loc, ">", 1) && strncmp(cur->loc, "&", 1))
+				fprintf(fout, "<symbol>%s</symbol>\n", strndup(cur->loc, 1));
+		}
 
 		if (cur->kind == TK_RESERVED)
-			fprintf(fout, "token_type: %s, keyword: %s\n", STR(TK_RESERVED), strndup(cur->loc, cur->len));
+			fprintf(fout, "<keyword>%s</keyword>\n", strndup(cur->loc, cur->len));
 
 		if (cur->kind == TK_STR)
-			fprintf(fout, "token_type: %s, string_literal: %s\n", STR(TK_STR), strndup(cur->contents, cur->cont_len));
+			fprintf(fout, "<stringConstant>%s</stringConstant>\n", strndup(cur->contents, cur->cont_len));
 
 		cur = cur->next;
 	}
+
+	fprintf(fout, "</tokens>");
 
 	fclose(fout);
 	return 0;
