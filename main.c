a9631f3d328661549fa5fd698d5562ab47b4f087
Hiroki Oshima <geet1876@gmail.com>
2020-05-20 05:37:20 +0900

    ファイルへ出力できるようにする

--- main.c
+++ main.c
@@ -36,24 +36,32 @@ int main(int argc, char **argv) {
 	char *input = read_file(argv[1]);
 	Token *cur = tokenize(input);
 
+	FILE *fout;
+	fout = fopen("Output.txt", "w");
+	if (fout == NULL) {
+		fprintf(stderr, "出力ファイルをオープンできません");
+		exit(1);
+	}
+
 	while (cur->kind != TK_EOF) {
 		if (cur->kind == TK_NUM)
-			printf("token_type: %s, value: %ld\n", STR(TK_NUM), cur->val);
+			fprintf(fout, "token_type: %s, value: %ld\n", STR(TK_NUM), cur->val);
 
 		if (cur->kind == TK_IDENT)
-			printf("token_type: %s, identifier: %s\n", STR(TK_IDENT), strndup(cur->loc, cur->len));
+			fprintf(fout, "token_type: %s, identifier: %s\n", STR(TK_IDENT), strndup(cur->loc, cur->len));
 
 		if (cur->kind == TK_SYMBOL)
-			printf("token_type: %s, symbol: %s\n", STR(TK_SYMBOL), strndup(cur->loc, 1));
+			fprintf(fout, "token_type: %s, symbol: %s\n", STR(TK_SYMBOL), strndup(cur->loc, 1));
 
 		if (cur->kind == TK_RESERVED)
-			printf("token_type: %s, keyword: %s\n", STR(TK_RESERVED), strndup(cur->loc, cur->len));
+			fprintf(fout, "token_type: %s, keyword: %s\n", STR(TK_RESERVED), strndup(cur->loc, cur->len));
 
 		if (cur->kind == TK_STR)
-			printf("token_type: %s, string_literal: %s\n", STR(TK_STR), strndup(cur->contents, cur->cont_len));
+			fprintf(fout, "token_type: %s, string_literal: %s\n", STR(TK_STR), strndup(cur->contents, cur->cont_len));
 
 		cur = cur->next;
 	}
 
+	fclose(fout);
 	return 0;
 }
